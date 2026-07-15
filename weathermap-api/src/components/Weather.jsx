3946595e0363684ff15c54b1c175b4a7345ac95c
alexander.leech <alexander.leech@capita.com>
2026-07-15 11:58:02 +0100

    weather card basics created, will move onto getting rough data from api next

--- weathermap-api/src/components/Weather.jsx
+++ weathermap-api/src/components/Weather.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
 
 const Weather = () => {
   return (
     <div className='weather'>
       <div className='search-input'>
         <input type="text" placeholder='Find my weather' />
-        <img src="" alt="Search Icon" />
+        <img src={search_icon} alt="Search Icon" />
       </div>
     </div>
   )
